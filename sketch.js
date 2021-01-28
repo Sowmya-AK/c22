a768c970a32a4ea2a882a8f4204c25a9feda9ef0
Sowmya-AK <sowmyaak04@gmail.com>
2021-01-28 23:49:34 +0530

    Added the new images and changes the background color

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopterIMG=loadImage("Helicopter.jpg");
+	packageIMG=loadImage("package.png");
 }
 
 function setup() {
@@ -17,15 +17,15 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.4;
 
 	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite.shapeColor=color(255);
 
 
 	engine = Engine.create();
@@ -47,9 +47,9 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  background("lightblue");
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
   drawSprites();
  
 }
